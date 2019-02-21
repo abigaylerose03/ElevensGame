297707ded5a901dd208aba243782e8ce3f886791
Emma <46979895+emmah4103@users.noreply.github.com>
2019-02-21 12:49:34 -0800

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -63,9 +63,19 @@ public class Deck {
      * Randomly permute the given collection of cards
      * and reset the size to represent the entire deck.
      */
-    public void shuffle() {
-        /* *** TO BE IMPLEMENTED IN ACTIVITY 4 *** */
+       public void shuffle() {
+        
+        	for(int k = cards.size(); k >= 1; k--)
+		{
+			Random ran = new Random();
+			int r = ran.nextInt(k);
+			Card temp = cards.get(r);
+			cards.get(r) = cards.get(k);
+			cards.get(k) = temp;
+		}
+        
     }
+
 
     /**
      * Deals a card from this deck.
